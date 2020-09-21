71ffabda432fdc51799382f38a684ae92b221674
kurniawa <kurniawan_01@ymail.com>
2020-09-21 15:16:27 +0700

    Masih berkutat di kotak2 opsi yang ada

--- 03-03-02-sj-basic.php
+++ 03-03-02-sj-basic.php
@@ -9,45 +9,58 @@
 
         <!-- <input id="variasi2" class="input-1 mt-1em pb-1em" type="text" placeholder="Variasi"> -->
 
-        <div class="grid-2-auto_10 mt-1em mb-0_5em">
+        <div class="grid-1-auto mt-1em mb-0_5em">
             <select name="variasi" id="variasi" class="pt-0_5em pb-0_5em" onchange="showNextVarian(this.value);">
                 <option value="" disabled selected>Pilih Variasi</option>
             </select>
-            <span class="ui-icon ui-icon-closethick justify-self-center"></span>
-
         </div>
 
+        <div id="divVariasiLG" class="grid-2-auto_10 mt-1em">
+            <select name="variasiLG" id="variasiLG" class="pt-0_5em pb-0_5em" onchange="showVariasiLG(this.value);">
+                <option value="" disabled selected>Pilih Variasi LOGO</option>
+            </select>
+            <span class="ui-icon ui-icon-closethick justify-self-center" onclick="closeVariasiLG();"></span>
+        </div>
 
-        <select name="variasiLG" id="variasiLG" class="mt-1em pt-0_5em pb-0_5em w-100" onchange="showVariasiLG(this.value);">
-            <option value="" disabled selected>Pilih Variasi LOGO</option>
-        </select>
+        <div id="divSelectVariasiBludru" class="grid-2-auto_10 mt-1em">
+            <select name="selectVariasiBludru" id="selectVariasiBludru" class="pt-0_5em pb-0_5em">
+                <option value="" disabled selected>Pilih Gambar Bludru</option>
+            </select>
+            <span class="ui-icon ui-icon-closethick justify-self-center" onclick="collapseAndReset(['#divSelectVariasiBludru', '#choseVariasiBludru'],['#selectVariasiBludru', '#variasiLG'])"></span>
+        </div>
 
-        <select name="variasiLGBludru" id="variasiLGBludru" class="mt-1em pt-0_5em pb-0_5em w-100">
-            <option value="" disabled selected>Pilih Variasi Bludru</option>
-        </select>
+        <div id="divJumlah">
+            <input type="number" name="jumlah" id="inputJumlah" min="0" step="1" placeholder="Jumlah" class="pt-0_5em pb-0_5em">
+        </div>
 
-        <select name="variasiTATO" id="variasiTATO" class="mt-1em pt-0_5em pb-0_5em w-100">
-            <option value="" disable selected>Pilih Variasi TATO</option>
-        </select>
-
+        <div id="divSelectVariasiTato" class="grid-2-auto_10 mt-1em">
+            <select name="variasiTato" id="variasiTato" class="pt-0_5em pb-0_5em">
+                <option value="" disable selected>Pilih Variasi TATO</option>
+            </select>
+            <span class="ui-icon ui-icon-closethick justify-self-center"></span>
+        </div>
 
         <br><br>
 
         <div id="warning" class="d-none"></div>
 
-
-        <div id="bottomDiv" class="position-absolute bottom-0_5em h-10em w-calc-100-1em">
+        <div id="divAvailableOptions" class="position-absolute bottom-5em">
             Available options:
-            <div id="availableOptions"></div>
+            <div id="availableOptions">
+                <div id="choseVariasiBludru" class="pt-0_5em pb-0_5em pl-1em pr-1em b-radius-10px bg-color-soft-red d-none">
+                    Variasi Bludru
+                </div>
+            </div>
+
+        </div>
+        <div id="bottomDiv" class="position-absolute bottom-0_5em w-calc-100-1em">
             <div class="h-4em bg-color-orange-2 grid-1-auto" onclick="insertNewProduct();">
                 <span class="justify-self-center font-weight-bold">TAMBAH ITEM KE SPK</span>
             </div>
         </div>
 
-
-
-
     </div>
+
 </div>
 
 <script>
@@ -58,9 +71,10 @@
 
     // console.log(testArray);
     $(document).ready(function() {
-        $("#variasiLG").css("display", "none");
-        $("#variasiLGBludru").css("display", "none");
-        $("#variasiTATO").css("display", "none");
+        $("#divVariasiLG").css("display", "none");
+        $("#divSelectVariasiBludru").css("display", "none");
+        $("#divSelectVariasiTato").css("display", "none");
+        $("#choseVariasiBludru").css("display", "none");
 
 
         fetch('json/products.json').then(response => response.json()).then(data => {
@@ -79,9 +93,9 @@
                 console.log(variasiLG.nama);
                 $("#variasiLG").append('<option value="' + variasiLG.nama + '">' + variasiLG.nama + '</option>');
             }
-            for (const variasiLGBludru of data[0].variasi[0].jenis_variasi[1].jenis_logo[0].variasi) {
+            for (const variasiLGBludru of data[0].variasi[0].jenis_variasi[1].jenis_logo[0].gambar) {
                 arrayVariasiLGBludru.push(variasiLGBludru);
-                $("#variasiLGBludru").append('<option value="' + variasiLGBludru + '">' + variasiLGBludru + '</option>');
+                $("#selectVariasiBludru").append('<option value="' + variasiLGBludru + '">' + variasiLGBludru + '</option>');
             }
         });
 
@@ -103,19 +117,56 @@
         console.log(value);
 
         if (value === "LG") {
-            $("#variasiLG").toggle()
-            $("#variasiTATO").css("display", "none");
+            $("#divVariasiLG").toggle();
+            $("#divVariasiTATO").css("display", "none");
         } else if (value === "TATO") {
-            $("#variasiTATO").toggle()
-            $("#variasiLG").css("display", "none");
+            $("#divVariasiTATO").toggle()
+            $("#divVariasiLG").css("display", "none");
         }
     }
 
     function showVariasiLG(value) {
         console.log(value);
         if (value === "Bludru") {
-            $("#variasiLGBludru").toggle();
+            $("#choseVariasiBludru").toggle();
         }
+    }
+
+    function showSelectVariasiBludru() {
+        $("#selectVariasiBludru").toggle();
+    }
+
+    function closeVariasiLG() {
+        $("#variasi").prop("selectedIndex", 0);
+        document.getElementById("variasiLG").selectedIndex = 0;
+        $("#divVariasiLG").css("display", "none");
+    }
+
+    document.getElementById("choseVariasiBludru").addEventListener('click', () => {
+        toggleShowElement(['#divSelectVariasiBludru']);
+        $("#choseVariasiBludru").css("display", "none");
+    });
+
+    document.getElementById('selectVariasiBludru').addEventListener('change', () => {
+
+    });
+
+    function toggleShowElement(idAll, time) {
+        idAll.forEach(id => {
+            if ($(id).css("display") === "none") {
+                $(id).toggle(time);
+            }
+        });
+    }
+
+    function collapseAndReset(idToCollapse, idToReset) {
+        idToReset.forEach(idElement => {
+            $(idElement).prop("selectedIndex", 0);
+            // document.getElementById(idElement).selectedIndex = 0;
+        });
+        idToCollapse.forEach(idElement => {
+            $(idElement).css("display", "none");
+        });
     }
 
     function insertNewProduct() {
